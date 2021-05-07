6609793b45849b997c2066aa5eb4c1d67861bcd0
James <jamesvo801@gmail.com>
2021-05-07 16:08:24 -0700

    Set up the first api call, set up search functionality, and set up anime cards that can redirect you to the original anime website

--- src/App.js
+++ src/App.js
@@ -1,12 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import Header from './components/Header';
+import Sidebar from './components/Sidebar';
+import MainContent from './components/MainContent';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
+	const [animeList, setAnimeList] = useState([]);
+	const [topAnime, setTopAnime] = useState([]);
+	const [search, setSearch] = useState("");
+
+	const getTopAnime = async () =>{
+		const tempAnime = await fetch(`https://api.jikan.moe/v3/top/anime/1/bypopularity`)
+							.then(res => res.json());
+		
+		setTopAnime(tempAnime.top.slice(0,5));
+	}
+
+	const HandleSearch = e => {
+		e.preventDefault();
+		console.log(search)
+		fetchAnime(search)
+	}
+
+	const fetchAnime = async (query) =>{
+		const temp = await fetch(`https://api.jikan.moe/v3/
+		search/anime?q=${query}&order_by=title&sort=asc&limit=10`)
+			.then(res => res.json());
+
+		setAnimeList(temp.results);
+	}
+
+	useEffect(() =>{
+		getTopAnime();
+		console.log("Top Anime");
+	}, []);
+
+	console.log(topAnime);
+
+    return (
+      	<div className="App">
+        	<Header/>
+			<div className="content-wrap">
+				<Sidebar topAnime={topAnime}/>
+				<MainContent HandleSearch={HandleSearch}
+				search={search}
+				setSearch={setSearch}
+				animeList={animeList}/>
+			</div>
+      	</div>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+
+function Header() {
+    return (
+        <h1><strong>Anime</strong>Database</h1>
+    )
+}
+
+export default Header
