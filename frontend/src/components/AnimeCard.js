3dc174661a299cbdf1b721ba746d48fa48979c92
James <jamesvo801@gmail.com>
2021-05-10 05:24:30 -0700

    Fixed CSS for the Modal
    Completed:
    -Fixed the CSS for the Modals so that they're the same size
    In Progress:
    -Work on the homepage with displaying top animes
    -Create static pages for now, then possibly move to dynamic

--- frontend/src/components/AnimeCard.js
+++ frontend/src/components/AnimeCard.js
@@ -5,7 +5,7 @@ const AnimeCard = ({ anime, index}) => {
 
     return (
         <article className="anime-card">
-            <button type="button" class="btn shadow-none" data-bs-toggle="modal" 
+            <button type="button" className="modal-button" data-bs-toggle="modal" 
             data-bs-target={`#animedetails-${index}`} >
                 <figure className="figure-outer-block">
                     <img className="image-inner-block" src={anime.image_url} alt="Anime Image" />
