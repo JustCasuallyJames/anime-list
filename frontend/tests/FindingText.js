233c218568da4eb75959778b168f01e9419c7af1
James <jamesvo801@gmail.com>
2021-05-24 00:06:58 -0700

    using a different ide

--- frontend/tests/FindingText.js
+++ frontend/tests/FindingText.js
@@ -8,5 +8,11 @@ module.exports = {
 	'Finding Anime Name' (browser){
 		browser.url("http://localhost:3001")
 			.assert.cssProperty(".anime-list", "display", "flex");
+	},
+
+	'Finding Steins Gate Name' (browser){
+		browser.url("http://localhost:3001")
+		.waitForElementVisible("#animedetails-5114", time=5000)
+		.assert.containsText("#animedetails-5114", "Fullmetal Alchemist: Brotherhood")
 	}
 }
